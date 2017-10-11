3e84901466484b8bc3bfb536b6ab80036f36621a
ontoneio <ontoneio.mm@gmail.com>
2017-10-10 21:40:36 -0700

    Part-1 route 1 working

--- part-1/app.js
+++ part-1/app.js
@@ -2,11 +2,26 @@ const express = require('express')
 const app = express()
 
 
-const log = require('morgan')
-app.use(log('dev'))
+
+const bodyParser = require('body-parser')
+const logging = require('morgan')
+
+app.use(logging('dev'))
+
 
 app.get(`/api/days/:day`, (request, response) => {
-  response.status(200).send(`This is the day of the week route!`)
+  const daysOfWeek = {monday: 1, tuesday:2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7}
+  const day = request.params.day.toLowerCase()
+  const routePath = request.route['path'].replace(/:day/, day)
+
+  if (!daysOfWeek[day]){
+    response.status(400).send(
+    `request: GET ${routePath}\nresponse: '${daysOfWeek[day]}' is not a valid day!\nstatus: 400`)
+  }
+  else {
+    response.status(200).send(
+    `request: ${routePath}\nresponse: ${daysOfWeek[day]}\nstatus: 200`)
+  }
 })
 
 app.post(`/api/array/concat`, (request, response) => {
