6b523069596bcc3f0b1e68fecb9bf886437342c2
ontoneio <ontoneio.mm@gmail.com>
2017-10-12 09:38:55 -0700

    cart button number update

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,13 +1,33 @@
 const modal = document.querySelector(".modal")
 const closeModal = document.querySelector(".close")
-const clearCart = document.querySelector("#clear-cart")
-const basket = document.querySelector("#basket")
+const clearCart = document.querySelector("clear-cart")
+const basket = document.querySelector("basket")
 
 const cartButton = document.querySelector("#cart-button")
-const addToCart = document.querySelector("#addItem")
+const addToCart = document.querySelectorAll("#addItem")
+const cartItemCount = document.querySelector('#cart-item-count')
 
-const itemsInCart = []
+let itemsInCart = 0
+let cartTotal = 0
 
 // OPEN MODAL - CLOSE MODAL 
 cartButton.addEventListener('click', () => modal.style.display = 'flex')
 closeModal.addEventListener('click', () => modal.style.display = 'none')
+
+// ADD ITEMS TO CART
+cartItemCount.innerHTML = `(${itemsInCart})`
+
+const addCartItems = () => {
+      
+}
+
+
+addToCart.forEach(button => {
+  button.addEventListener('click', () => {
+    let updatedNumber = itemsInCart += 1
+    cartItemCount.innerHTML = updatedNumber    
+    console.log(`YOU ADDED AN ITEM! ${updatedNumber}`);
+  })
+})
+
+
